162b7c2a81ef702efabbf2b71931e87797a2aeb5
issacdowling <12268666-issacdowling@users.noreply.gitlab.com>
2022-12-24 15:45:20 +0000

    Initialise server file from previous tests

---
+++ Server/server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const serverPort = 3000,
+    http = require("http"),
+    express = require("express"),
+    app = express(),
+    server = http.createServer(app),
+    WebSocket = require("ws"),
+    websocketServer = new WebSocket.Server({ server });
+
+//when a websocket connection is established
+websocketServer.on('connection', (webSocketClient) => {
+    //send feedback to the incoming connection
+    // //webSocketClient.send('{ "connection" : "ok"}');
+
+    webSocketClient.send("MYrfLmm_cT4")
+    
+    //when a message is received
+    webSocketClient.on('message', (message) => {
+
+        console.log(message)
+
+        //for each websocket client
+        websocketServer
+        .clients
+        .forEach( client => {
+            //send the client the current message
+            client.send(`{ "message" : ${message} }`);
+        });
+    });
+});
+
+//start the web server
+server.listen(serverPort, () => {
+    console.log(`Websocket server started on port ` + serverPort);
+});
