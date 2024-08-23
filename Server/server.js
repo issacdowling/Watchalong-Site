ad13a78e8d53d49c3a87327647464584b4dad17f
issacdowling <git@issacdowling.com>
2024-08-23 14:49:13 +0100

    re-style all files

--- Server/server.js
+++ Server/server.js
@@ -1,47 +1,39 @@
 "use strict";
 
 const serverPort = 3000,
-    http = require("http"),
-    express = require("express"),
-    app = express(),
-    server = http.createServer(app),
-    WebSocket = require("ws"),
-    websocketServer = new WebSocket.Server({ server });
+  http = require("http"),
+  express = require("express"),
+  app = express(),
+  server = http.createServer(app),
+  WebSocket = require("ws"),
+  websocketServer = new WebSocket.Server({ server });
 
-
-
+function sendRandomVideo(wsc) {
+  let videoIDs = ["MYrfLmm_cT4", "i_mLxyIXpSY", "8mrNEVUuZdk", "OCJYDJXDRHw"];
+  let videoID = videoIDs[Math.floor(Math.random() * videoIDs.length)];
+  wsc.send(videoID);
+  return videoID;
+}
 
 //when a websocket connection is established
-websocketServer.on('connection', (webSocketClient) => {
+websocketServer.on("connection", (webSocketClient) => {
+  console.log("New connection");
+  // Send a video every five seconds
+  setInterval(sendRandomVideo, 5000, webSocketClient);
 
-    function send_random_video() {
-        let videoIDs = ["MYrfLmm_cT4", "i_mLxyIXpSY", "8mrNEVUuZdk", "OCJYDJXDRHw"]
-        let videoID = videoIDs[Math.floor(Math.random() * videoIDs.length)];
-        webSocketClient.send(videoID)
-        return videoID
-    }
+  //when a message is received, echo it
+  webSocketClient.on("message", (message) => {
+    console.log(message);
 
-    // Send a video every five seconds
-    setInterval(send_random_video, 5000)
-
-    setTimeout(get_video, 10000)
-    
-    //when a message is received, echo it
-    webSocketClient.on('message', (message) => {
-
-        //console.log(message)
-
-        //for each websocket client
-        websocketServer
-        .clients
-        .forEach( client => {
-            //send the client the current message
-            //client.send(`{ "message" : ${message} }`);
-        });
+    //for each websocket client
+    websocketServer.clients.forEach((client) => {
+      //send the client the current message
+      //client.send(`{ "message" : ${message} }`);
     });
+  });
 });
 
 //start the web server
 server.listen(serverPort, () => {
-    console.log(`Websocket server started on port ` + serverPort);
+  console.log(`Websocket server started on port ` + serverPort);
 });
