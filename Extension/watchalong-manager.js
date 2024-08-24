0469263c2a310d21ec499a7ab237cdc7b97662ac
issacdowling <git@issacdowling.com>
2024-08-24 14:33:05 +0100

    Support finding video IDs in playlists

--- Extension/watchalong-manager.js
+++ Extension/watchalong-manager.js
@@ -2,6 +2,18 @@ console.log("Watchalong extension loaded");
 
 let url = document.location.href;
 let watchStr = "watch?v=";
-let videoId = url.slice(url.indexOf(watchStr) + watchStr.length);
+
+// I'm operating under the assumption that the & symbol won't appear in video IDs,
+// since that's how they separate other query parameters.
+// Checking for (and ending at) an & means allowing this to work with playlists.
+let locationOfAmpersand = url.indexOf("&");
+if (locationOfAmpersand == -1) {
+  locationOfAmpersand = url.length;
+}
+
+let videoId = url.slice(
+  url.indexOf(watchStr) + watchStr.length,
+  locationOfAmpersand,
+);
 
 console.log("Watchalong extension found the video ID: " + videoId);
