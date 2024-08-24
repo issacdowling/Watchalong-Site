fd4e2d86d3039a73e78f1e6fc697230fb798bd2c
issacdowling <git@issacdowling.com>
2024-08-24 15:38:42 +0100

    Detect changes in video ID

--- Extension/watchalong-manager.js
+++ Extension/watchalong-manager.js
@@ -1,19 +1,39 @@
-console.log("Watchalong extension loaded");
+console.log("[Watchalong Manager] Loaded");
 
-let url = document.location.href;
-let watchStr = "watch?v=";
+let currentId;
 
-// I'm operating under the assumption that the & symbol won't appear in video IDs,
-// since that's how they separate other query parameters.
-// Checking for (and ending at) an & means allowing this to work with playlists.
-let locationOfAmpersand = url.indexOf("&");
-if (locationOfAmpersand == -1) {
-  locationOfAmpersand = url.length;
-}
+const extractIdFromUrl = (url) => {
+  let watchStr = "watch?v=";
 
-let videoId = url.slice(
-  url.indexOf(watchStr) + watchStr.length,
-  locationOfAmpersand,
-);
+  // I'm operating under the assumption that the & symbol won't appear in video IDs,
+  // since that's how they separate other query parameters.
+  // Checking for (and ending at) an & means allowing this to work with playlists.
+  let locationOfAmpersand = url.indexOf("&");
+  if (locationOfAmpersand == -1) {
+    locationOfAmpersand = url.length;
+  }
 
-console.log("Watchalong extension found the video ID: " + videoId);
+  return url.slice(
+    url.indexOf(watchStr) + watchStr.length,
+    locationOfAmpersand,
+  );
+};
+
+// Global state somewhat unideal (currentId) but seems to make everything cleaner in this case
+// Also, I wanted to pass in the URL too, but setInterval seems to lock href to whatever it was at page load,
+// where including it in this function lets it change.
+const checkURLChange = () => {
+  let foundId = extractIdFromUrl(document.location.href);
+  if (foundId == currentId) {
+    return;
+  }
+
+  currentId = foundId;
+  console.log(
+    "[Watchalong Manager] URL CHANGED, found the video ID: " + foundId,
+  );
+};
+
+// Doesn't seem like the most efficient way to do this, but any other
+// ways I found added much more complexity
+setInterval(checkURLChange, 1000);
